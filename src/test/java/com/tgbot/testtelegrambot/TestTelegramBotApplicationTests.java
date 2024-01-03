623372337c417da1778cc8ed115500a7f50fa685
Koshkin Nazar <mr.kisskin@gmail.com>
2024-01-03 08:26:18 +0300

    Initial commit

---
+++ src/test/java/com/tgbot/testtelegrambot/TestTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgbot.testtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
